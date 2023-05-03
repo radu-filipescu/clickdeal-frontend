029319afbdb1428079a830f955f9b2db9ded84d2
radu-filipescu <radu.filipescu@s.unibuc.ro>
2023-05-03 16:54:59 +0300

    added login page script (not working yet)

---
+++ javascripts/login-page/login-script.js
@@ -0,0 +1,60 @@
+// run this code after window loads,
+// so we don't get null elements on getElementById
+
+window.onload = init;
+
+function init() {
+    const submitButton = document.getElementById('submit-button-ID');
+
+    submitButton.addEventListener('click', handleFormSubmit);
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const form = document.getElementById('login-form');
+
+    const formData = new FormData(form);
+    let data = Array.from(formData.entries())
+        .reduce((obj, [key, value]) => {
+        obj[key] = value;
+
+        return obj;
+    }, {});
+
+    data = JSON.stringify(data);
+    let jsonData = JSON.parse(data);
+
+    jsonData["rememberMe"] = false;
+
+    if(jsonData.rememberMe == "on")
+        jsonData["rememberMe"] = true;
+        
+
+    const loginUrl = 'https://localhost:44396/api/account/login';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', loginUrl);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        
+        console.log(response);
+        } 
+        else {
+          console.log('shit happened');
+        }
+    };
+    
+    xhr.onerror = function() {
+        console.error('An error occurred while sending the request.');
+    };   
+
+    console.log('sending data:', jsonData);
+
+    xhr.send(jsonData);
+}
+
+
+
