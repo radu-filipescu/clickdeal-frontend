2b8f6cde60c84b2969b99d6b8dcf30d5ae6c669f
radu-filipescu <radu.filipescu@s.unibuc.ro>
2023-05-05 01:32:14 +0300

    fixed navbar and copied it on every page

--- javascripts/header-logic/header-script.js
+++ javascripts/header-logic/header-script.js
@@ -1,12 +1,34 @@
-import { isLoggedIn } from "../shared/CONSTS.js"
+import { isLoggedIn } from "../shared/utils.js"
+import { CONSTS } from "../shared/CONSTS.js"
 
 window.onload = init;
 
+function profileClickLogged() {
+    // TODO: navigate to profile page
+}
+
 function init() {
+    const profileButton = document.getElementById('profile-button');
+    const shoppingCartButton = document.getElementById('shopping-cart-button');
+
     isLoggedIn( () => {
-        console.log('true');
+        // true callback (what to do if user is logged)
+        console.log('is logged in');
+
+        // profile button goes to profile page
+        profileButton.removeEventListener("click");
+
+        profileButton.addEventListener("click", profileClickLogged);
     }, 
     () => {
-        console.log('false');
+        // false callback (what to do if user is not logged)
+        console.log('in not logged');
+
+        // profile button goes to login page
+        profileButton.removeEventListener("click");
+
+        profileButton.addEventListener("click", () => {
+            window.location.href = CONSTS.frontendDevLogin;
+        });
     });
 }
