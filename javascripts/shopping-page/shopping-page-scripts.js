c6a918a4e645f84579aff099a0305036afada05d
radu-filipescu <radu.filipescu@s.unibuc.ro>
2023-05-22 17:02:28 +0300

    category filter implemented visually in shop page

--- javascripts/shopping-page/shopping-page-scripts.js
+++ javascripts/shopping-page/shopping-page-scripts.js
@@ -9,5 +9,57 @@ function init() {
 }
 
 function getCategoriesFilter() {
+    const categoriesUrl = CONSTS.URLS.backendDevUrl + 'app/categories/categories';
 
+    fetch(categoriesUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include' // include cookies in the request
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        return response.json();
+    })
+    .then(data => {
+        // set categories names to filters
+        const categoriesFilter = document.getElementById('categories-filter');
+
+        console.log(data);
+        for(let i = 0; i < data.length; i++) {
+            // <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+            //      <input type="checkbox" class="custom-control-input" checked id="color-all">
+            //      <label class="custom-control-label" for="price-all">All Color</label>
+            //      <span class="badge border font-weight-normal">1000</span>
+            // </div>
+            
+            let div1 = document.createElement('div');
+            div1.className = "custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3";
+
+                let input1 = document.createElement('input');
+                input1.className = "custom-control-input";
+
+                let label1 = document.createElement('label');
+                label1.className = "custom-control-label";
+                label1.innerText = data[i].name;
+
+                let span1 = document.createElement('span');
+                span1.className = "badge border font-weight-normal";
+                span1.innerText = data[i].productsNumber;
+
+            
+            div1.appendChild(input1);
+            div1.appendChild(label1);
+            div1.appendChild(span1);
+
+            categoriesFilter.appendChild(div1);
+        }      
+    })
+    .catch(error => {
+        console.error('There was a problem with getting categories:', error);
+    });
 }
